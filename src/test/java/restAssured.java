926a5dc04a31e96a31509802be52654b50b2ca44
CORP\aashok <ardra.ashok@transport.nsw.gov.au>
2022-10-03 22:21:58 +1100

    added put,post,patch & delete request

--- src/test/java/restAssured.java
+++ src/test/java/restAssured.java
@@ -1,20 +1,83 @@
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
-import org.testng.Assert;
+import static  io.restassured.RestAssured.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItems;
+
+
+import io.restassured.http.ContentType;
+import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class restAssured {
 
    @Test
     public static void restAssured(){
-       Response response = RestAssured.get("https://reqres.in/api/users?page=2");
-       System.out.println(response.asString());
-       System.out.println(response.getBody().asString());
-       System.out.println(response.getStatusCode());
-       System.out.println(response.getStatusLine());
-       System.out.println(response.getHeader("content-type"));
-       System.out.println(response.getTime());
-       int statusCode = response.getStatusCode();
-       Assert.assertEquals(200,statusCode);
+//     get request
+       given()
+           .header("Content-type","application/json")
+           .get("https://reqres.in/api/users?page=2")
+       .then()
+           .statusCode(200)
+           .body("data.id[1]",equalTo(8))
+           .body("data.first_name",hasItems("Michael","Lindsay"))
+           .log().all();
+
+//     post request
+
+       JSONObject req = new JSONObject();
+       req.put("name","Priya");
+       req.put("job","Programmer");
+       given()
+            .header("Content-type","application/json")
+            .contentType(ContentType.JSON).accept(ContentType.JSON)
+            .body(req.toJSONString())
+       .when()
+            .header("Content-type","application/json")
+            .post("https://reqres.in/api/users")
+       .then()
+            .statusCode(201)
+            .log().all();
+//     put request
+       req = new JSONObject();
+       req.put("name","Priya");
+       req.put("job","Dentist");
+       given()
+               .header("Content-type","application/json")
+               .contentType(ContentType.JSON).accept(ContentType.JSON)
+               .body(req.toJSONString())
+               .when()
+               .header("Content-type","application/json")
+               .put("https://reqres.in/api/users/2")
+               .then()
+               .statusCode(200)
+               .log().all();
+
+//     patch request
+       req = new JSONObject();
+       req.put("name","Priya");
+       req.put("job","Dentist");
+       given()
+               .header("Content-type","application/json")
+               .contentType(ContentType.JSON).accept(ContentType.JSON)
+               .body(req.toJSONString())
+               .when()
+               .header("Content-type","application/json")
+               .patch("https://reqres.in/api/users/2")
+               .then()
+               .statusCode(200)
+               .log().all();
+//     delete request
+       given()
+               .header("Content-type","application/json")
+               .contentType(ContentType.JSON).accept(ContentType.JSON)
+               .when()
+               .header("Content-type","application/json")
+               .put("https://reqres.in/api/users/2")
+               .then()
+               .statusCode(200)
+               .log().all();
+//
     }
 }
